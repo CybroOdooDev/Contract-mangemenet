2edfab374a8044837fab4734fdecba6585264559
Haricm1 <haricm2017@gmail.com>
2022-09-27 14:47:30 +0530

    [ADD]version updated

--- xf_partner_contract/__manifest__.py
+++ xf_partner_contract/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Contract Management',
-    'version': '1.0.0',
+    'version': '15.0.1.1.0',
     'summary': """
     This module helps to manage/approve/renew contracts
     , purchase contract 
