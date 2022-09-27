00706735fd6788d11c66e701d4fc06d5ba898efa
Haricm1 <haricm2017@gmail.com>
2022-09-27 14:08:37 +0530

    [ADD]updated

---
+++ xf_partner_contract/static/src/js/demo.js
@@ -0,0 +1,38 @@
+odoo.define('xf_partner_contract.demo', function (require) {
+'use strict';
+
+const components = {
+    demo: require('documents.controllerMixin'),
+};
+
+const { patch } = require('web.utils');
+
+patch(components.demo, 'xf_partner_contract.demo', {
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    _getFileUploadRoute() {
+        console.log('demo1')
+        if (this.context.active_ids && this.context.active_model=='xf.partner.contract')  {
+            var active_id = this.context.active_ids[0];
+        }else{
+            var active_id = 0;
+        };
+        var url = ('/documents/upload_attachment/'+ active_id);
+        return url;
+    },
+
+
+    _onClickDocumentsUpload(ev) {
+        console.log('demo2')
+        this.context = this.model.get(this.handle).getContext();
+        this._uploadFilesHandler(true)(ev);
+    },
+});
+
+});
